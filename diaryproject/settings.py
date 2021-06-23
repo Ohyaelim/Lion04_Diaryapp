3f08825297b45c375fa4c644d429252b6dbbb40c
Ohyaelim <npr05080@gmail.com>
2021-06-24 00:38:26 +0900

    7차 세션 과제

--- diaryproject/settings.py
+++ diaryproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'photo', 'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_URL='/static/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
 
 # Application definition
 
@@ -39,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'bootstrap4',
+    'photo',
 ]
 
 MIDDLEWARE = [
